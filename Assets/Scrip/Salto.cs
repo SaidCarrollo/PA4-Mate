f926ba75246f22c91037df42810623ddad6515f9
SaidCarrollo <150976435+SaidCarrollo@users.noreply.github.com>
2024-06-13 09:45:55 -0500

    Nivel 1
    en proceso

--- Assets/Scrip/Salto.cs
+++ Assets/Scrip/Salto.cs
@@ -4,51 +4,69 @@ using UnityEngine;
 
 public class Salto : MonoBehaviour
 {
-    private Rigidbody rig;
+    private Rigidbody _comRigiBody;
+    private bool detector;
+    public LayerMask Detectar;
+    public float raycast;
+    public float SpeedX;
     public float salto;
-    public float verti;
-    public bool detector;
-    public bool jum;
-    // Start is called before the first frame update
-    void Start()
+    public int maxSaltos = 2;
+    private int saltosRestantes;
+
+    private void Start()
     {
-        rig = GetComponent<Rigidbody>();
+        _comRigiBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        verti = Input.GetAxis("Vertical");
-
-        if (Input.GetAxis("Vertical") > 0 && detector == true)
-        {
-            jum = true;
-        }
-        else
-        {
-            jum = false;
-        }
-
+        CheckGround();
     }
+
     private void FixedUpdate()
     {
+        Move();
+    }
 
-        if (jum == true)
+    private void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0);
+        _comRigiBody.velocity = new Vector2(moveInput.x * SpeedX, _comRigiBody.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && saltosRestantes > 0)
         {
-            rig.AddForce(Vector2.up * salto, ForceMode.Impulse);
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        _comRigiBody.velocity = new Vector2(_comRigiBody.velocity.x, 0);
+        _comRigiBody.AddForce(Vector2.up * salto, ForceMode.Impulse);
+        saltosRestantes--;
+    }
+
+    private void CheckGround()
+    {
+        bool isGrounded = Physics2D.Raycast(transform.position, Vector2.down, raycast, Detectar);
+
+        if (isGrounded)
+        {
+            saltosRestantes = maxSaltos;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "piso")
+        if (collision.gameObject.CompareTag("piso"))
         {
             detector = true;
         }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "piso")
+        if (collision.gameObject.CompareTag("piso"))
         {
             detector = false;
         }
